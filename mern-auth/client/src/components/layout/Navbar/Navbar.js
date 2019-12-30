8b292d30a479f6aed313d552d0c02af97c40ee14
manish <manish.singh@ymedialabs.com>
2019-12-30 08:02:08 +0530

    added less loader

--- mern-auth/client/src/components/layout/Navbar/Navbar.js
+++ mern-auth/client/src/components/layout/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./Navbar.less";
 class Navbar extends Component {
   render() {
     return (
